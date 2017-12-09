6447dbbdcc83ad36286cf0a1cc416d16cf557cc6
Arshia <arshiakohanteb@gmail.com>
2017-12-08 17:32:07 -0800

    Initial Commit

---
+++ Queued Linked List/QueueLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queued_Linked_List
+{
+    class QueueLinkedList<T>
+    {
+        bool isEmpty = true;
+        public QNode<T> Head = null;
+        public QueueLinkedList()
+        {
+            Head = null;
+        }
+        #region ENQ
+        public void EnQ(T Value)
+        {
+            QNode<T> NewNode = new QNode<T>(Value);
+            if (Head == null)
+            {
+                Head = new QNode<T>(Value);
+            }
+            else if (Head.NextNode == null)
+            {
+                Head.NextNode = new QNode<T>(Value);
+            }
+            else
+            {
+                QNode<T> traversalNode = Head;
+                while (traversalNode.NextNode != null)
+                {
+                    traversalNode = traversalNode.NextNode;
+                }
+                traversalNode.NextNode = new QNode<T>(Value);
+            }
+        }
+        #endregion
+        #region DEQ
+        public T DeQ()
+        {
+            QNode<T> temp = Head;
+            Head.Previous = Head;
+            return;
+        }
+        #endregion
+    }
+}
---
+++ Queued Linked List/QueueNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queued_Linked_List
+{
+    class QNode <T>
+    {
+        public T Data;
+        public QNode<T> NextNode;
+        public QNode<T> Previous;
+        public QNode(T data)
+        {
+            Data = data;
+            NextNode = null;
+            Previous = null;
+        }
+    }
+}
